b3067192f1acc64beead139fd09988637b00ee5b
ZeynabAliyeva <103027531+ZeynabAliyeva@users.noreply.github.com>
2022-09-05 15:16:25 +0400

    tasks

--- task-1/main.js
+++ task-1/main.js
@@ -6,13 +6,12 @@ prompt.get('input', function (err, result) {
   
     let n = result.input.split(',')
     let sum = 0;
-    let index = 0;
-    let sum_division = 0; 
+    let n_length = n.length;
+    
     for(let i=0; i<n.length; i++){
         sum+=parseInt(n[i]) //18
-        index = n.length
-        sum_division = parseInt(sum/index); //3
     }
+    let sum_division = parseInt(sum/n_length ); //3
 
     let sum_second = 0;
     let count = 0;
--- task-4/main.js
+++ task-4/main.js
@@ -5,13 +5,24 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-  let n = result.input.split(',')
+  let n = result.input.split(",")
   let arr = [];
 
-    for (let i = 0; i < n.length - 1; i++) {
-      if (n[i + 1] !== n[i]) {
-        arr.push(n[i]);
-      }
-    }
-        console.log(arr);
+  for(let i = 0; i <n.length; i++){
+  const element = parseInt(n[i])
+  let exist = false;
+
+      for(let j = 0; j < arr.length; j++){
+          const next_element = parseInt(n[j])
+          if(element==next_element){
+
+            exist = true;
+            break;
+          }
+  }
+  if(!exist){
+    arr.push(element);
+  }
+}
+  console.log(arr.join(" "));
 });
